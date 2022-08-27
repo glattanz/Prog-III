2ba5b17f2323fd0de75c69073fe0596831027570
Gabrielle <g.lattanzi26@gmail.com>
2022-08-27 14:04:50 -0300

    Criando projeto banco

---
+++ Banco/src/main/java/br/edu/femass/model/Conta.java
@@ -0,0 +1,71 @@
+package br.edu.femass.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Conta {
+
+    private Long numero;
+    private Double saldo;
+    private Double especial;
+    private List<Historico> historicos;
+
+    private static Long proximoNumero = 1L;
+
+    public Conta(Double especial){
+        this.numero = proximoNumero;
+        proximoNumero++;
+        this.saldo = 0.0;
+        this.especial = especial;
+        this.historicos = new ArrayList<Historico>();
+    }
+
+    public void debitar(Double valor) {
+        if (valor>this.saldo+ this.especial) {
+            throw new IllegalArgumentException("Saldo Insuficiente");
+        }
+        this.saldo-=valor;
+        Historico historico = new Historico(valor, TipoLancamento.DEBITO);
+        this.historicos.add(historico);
+    }
+
+    public void creditar(Double valor) {
+        this.saldo+=valor;
+        Historico historico = new Historico(valor, TipoLancamento.CREDITO);
+        this.historicos.add(historico);
+    }
+
+    public String getHistorico() {
+        String resp = "";
+        for (Historico historico: historicos) {
+            resp+=historico.toString() + "\n";
+        }
+
+        return resp;
+    }
+
+    public Double getEspecial() {
+        return especial;
+    }
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public List<Historico> getHistoricos() {
+        return historicos;
+    }
+
+    public void setEspecial(Double especial) {
+        this.especial = especial;
+    }
+
+    @Override
+    public String toString(){
+        return this.numero.toString();
+    }
+}
---
+++ Banco/src/main/java/br/edu/femass/model/TipoLancamento.java
@@ -0,0 +1,13 @@
+package br.edu.femass.model;
+
+public enum TipoLancamento {
+    DEBITO("Débito"),
+    CREDITO("Crédito");
+
+    String valor;
+
+    TipoLancamento(String valor){
+        this.valor = valor;
+    }
+
+}
